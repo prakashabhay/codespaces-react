85478a37c24c9204aff0cfed1c5fad94580419b3
Prakash Abhay <prakashabhay3040@gmail.com>
2023-02-24 07:17:39 +0000

    new signature react canvas

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import './App.css';
-
+import React, {useRef, useState, useEffect } from 'react';
+import SignatureCanvas from 'react-signature-canvas';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    const sigRef = useRef()
+    const [signature, setSignature] = useState(null)
+    const handleSignatureEnd = () => {
+      setSignature(sigRef.current.toDataURL())
+    }
+    const clearSignature = () => {
+      sigRef.current.clear();
+      setSignature(null);
+    }
+  
+    useEffect(() => {
+      console.log(signature);
+    }, [signature])
+
+    return (<div>
+      <SignatureCanvas
+        canvasProps={{className: 'signature'}}
+        ref={sigRef}
+        onEnd={handleSignatureEnd}
+      />
+      <button onClick={clearSignature}>Edit</button>
     </div>
-  );
+  )
 }
 
 export default App;
